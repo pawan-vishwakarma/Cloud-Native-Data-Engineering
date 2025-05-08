3b10fd01eac840270338282de2c50b65a359b7c6
Pawan Vishwakarma <73687222+pawan-vishwakarma@users.noreply.github.com>
2025-05-08 12:59:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load from .env file
+
+GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
+BQ_DATASET = os.getenv("BQ_DATASET")
+BQ_TABLE = os.getenv("BQ_TABLE")
+GCS_OBJECT_PATH = os.getenv("GCS_OBJECT_PATH")
+LOCAL_FILE_PATH = os.getenv("LOCAL_FILE_PATH")
 
